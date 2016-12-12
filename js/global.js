9a339f57f764160b2c26360dbce790e3b88a0bca
Abello966 <antonio.augusto.abello@gmail.com>
2016-12-11 22:49:23 -0200

    Cookies funcionando

--- js/global.js
+++ js/global.js
@@ -1,33 +1,36 @@
 /*Lida com I/O de cookies (faz intermediario com API) 
-/*Inicializa variaveis globais
-/*Salva elas no cookie */
+/* - Inicializa variaveis globais
+/* - Salva elas no cookie 
+/* - Guarda constantes */
 
+window.ndays = 7;
+window.nhorarios = 24;
 
 //globais: semana e disciplinas
-var global {
+var global = {
+    origin: null, //data de origem
+
     open: function() {
         /*Abre ou chama inicializador se é a primeira vez*/
-        if ((sem = Cookies.get('semana')) != undefined)
-            window.semana = sem;
-        else 
-            window.semana = SemanaBootstrap(); //definido em semana.js
-        
-        /*disciplina nao precisa inicializar se nao tem*/
-        if ((disc = Cookies.get('disciplinas')) != undefined)
-            window.disciplinas = [];
-
-        /*Salva em caso de primeira vez*/
-        if (sem == undefined) 
+        if (Cookies.get('origin')) {
+            /*já existe*/
+            window.semana = Cookies.get('semana');
+            window.disciplinas = Cookies.get('disciplinas');
+            console.log("Dados carregados de: "+ Cookies.get('origin'));
+        }
+        else {
+            window.semana = SemanaBootstrap();
             this.save();
-        
-    }
+        }   
+    },
     
     /* atualiza cookie */
     save: function() {
         /*Cookie expira em 7 dias */
         Cookies.set('semana', window.semana, {expires: 7})
         Cookies.set('disciplinas', window.disciplinas, {expires: 7})    
-    }
+        Cookies.set('origin', new Date());
+    },
 }
                 
 
